494a7190167021c65b35c3e5e8b9f941e0f632fb
Diogo Franco <web@diogofranco.com.br>
2019-09-25 19:57:12 -0300

    created route and index

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import { View } from 'react-native';
+import BottomNavigation from 'react-native-material-bottom-navigation';
+
+const App = () => {
+    return (
+      <View>
+        <BottomNavigation />
+      </View>
+    );
+};
+
+export default App;
---
+++ src/routes.js
@@ -0,0 +1,133 @@
+import React, {Component} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import BottomNavigation, {
+    FullTab
+  } from 'react-native-material-bottom-navigation';
+
+const App = () => {
+  return (
+    <Component>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <Header />
+          {global.HermesInternal == null ? null : (
+            <View style={styles.engine}>
+              <Text style={styles.footer}>Engine: Hermes</Text>
+            </View>
+          )}
+          <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Step One</Text>
+              <Text style={styles.sectionDescription}>
+                Edit <Text style={styles.highlight}>App.js</Text> to change this
+                screen and then come back to see your edits.
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>See Your Changes</Text>
+              <Text style={styles.sectionDescription}>
+                <ReloadInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Debug</Text>
+              <Text style={styles.sectionDescription}>
+                <DebugInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Learn More</Text>
+              <Text style={styles.sectionDescription}>
+                Read the docs to discover what to do next:
+              </Text>
+            </View>
+            <LearnMoreLinks />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </Component>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App {
+    tabs = [
+        {
+          key: 'games',
+          icon: 'gamepad-variant',
+          label: 'Games',
+          barColor: '#388E3C',
+          pressColor: 'rgba(255, 255, 255, 0.16)'
+        },
+        {
+          key: 'movies-tv',
+          icon: 'movie',
+          label: 'Movies & TV',
+          barColor: '#B71C1C',
+          pressColor: 'rgba(255, 255, 255, 0.16)'
+        },
+        {
+          key: 'music',
+          icon: 'music-note',
+          label: 'Music',
+          barColor: '#E64A19',
+          pressColor: 'rgba(255, 255, 255, 0.16)'
+        }
+      ]
+}
